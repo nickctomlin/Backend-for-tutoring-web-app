65862dd3e39eb3d90b8b59700a3b25b7b483e892
Ayush-Gopisetty <ayush.gopisetty@gmail.com>
2023-04-28 21:07:11 -0500

    Fixed and updated front-end to work along with the login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from './Navigation';
+import Home from './Home';
+import AboutUs from './AboutUs';
+import Contact from './Contact'
+import Header from "./Header";
 import { useAuth0, isAuthenticated} from "@auth0/auth0-react";
 
-const App = () => {
+function App()  {
   const { loginWithRedirect,loginWithPopup, isAuthenticated } = useAuth0();
 
-  return <div>
-    {!isAuthenticated && <button onClick={() => loginWithRedirect()}>Log Into Account</button>}
-  </div>;
-  //return <button onClick={() => loginWithPopup()}>Log In With loginWithPopup</button>;
+  return ( 
+  <div>
+    <div><Navigation>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<AboutUs />} />
+              <Route exact path="/contact" element={<Contact />} />
+            </Routes>
+          </Navigation>
+    <Header/>
+    <Home/>
+    <AboutUs/>
+    <Contact/>
+    </div>
+    <div>{!isAuthenticated && <button onClick={() => loginWithRedirect()}>Log Into Account</button>}</div>
+  </div>
+  //return <button onClick={() => loginWithPopup()}>Log In With loginWithPopup</button>
+  );
 };
 
 export default App;
