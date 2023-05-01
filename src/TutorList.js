37ff0b1bb58e9a58c4127c3a4a06377429e2eaca
Padmaja Dronavalli <padronaval@MB-C02ZJ71WMD6M.local>
2023-05-01 15:58:05 -0500

    Added .map

--- src/TutorList.js
+++ src/TutorList.js
@@ -1,85 +1,65 @@
 import React, { useState, useEffect } from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
-
 const TutorList= () => {
+  const { logout,isAuthenticated  } = useAuth0();
     var t
-    const [tutors, setTutors] = useState([{"tutorID":"Sam"}],[{"tutorID":"Gopi"}]);
+    const [tutors, setTutors] = useState(<div></div>);
     
     useEffect(() => {
-        callApi()
-    });
-    async function callApi(){
-        try{
-            const response = await axios.get('http://localhost:4000/tutors');
-            setTutors(response.data);
-            console.log(setTutors[0].tutorID)
-        }
-        catch{
-            console.log("error")
-        }
-    }
-    return (
-      <div>
-      <section className="page-section bg-light" id="tutorlist">
-  <h1><center>Tutoring Listing</center></h1>
-  <div className="flex-container">
-  <a href="#">
-        <div>{tutors[0].tutorID}
-          <a href="#"></a>
-          <div className="about-me">
-            <div></div>
-            <b>About Me</b>: UT Dallas Engineering Professor
-            <br></br>
-            <b>Expertise</b>: Requirements Engineering
-          </div>
-        </div>
-        <br></br>
-        </a>
-        <a href="#">
-            <div>Professor Nhut
-            <a href="#">
-                <div class="about-me">
-                <div></div>
-                <b>About Me</b>: UT Dallas Engineering Professor
-                <br></br>
-                <b>Expertise</b>: Networks
-                </div>
-            </a>
-            </div>
-        </a>
+      callApi().then(()=>{
+       //console.log(checker);
+     if (t != null) {
+               console.log("Here At Not Tutor List")
+               console.log(t);
+             setTutors(          <div className="flex-container">
 
+              {t.map(tutor => (
+                
         <a href="#">
-        <div>Professor Paulk
+        <div>{tutor.tutorId}
           <a href="#">
             <div class="about-me">
               <div></div>
-              <b>About Me</b>: UT Dallas Engineering Professor
+              <b>About Me</b>: {tutor.aboutMe} 
               <br></br>
               <b>Expertise</b>: Project Managment
             </div>
             </a>
         </div> 
       </a>
-
-      <a href="#">
-        <div>Professor Khan
-          <a href="#">
-            <div class="about-me">
-              <div></div>
-              <b>About Me</b>: UT Dallas Engineering Professor
-              <br></br>
-              <b>Expertise</b>:  Software Engineering
+              ))}
             </div>
-            </a>
-        </div> 
-      </a>
-
-
-
-
-  </div>
+            
+            );
+           } else
+           {
+            console.log("Mistake");
+           }
+   
+   });
+     }, [isAuthenticated]);
+    async function callApi(){
+      try {
+          
+          //console.log("Is Authooo")
+  //const token = await getAccessTokenSilently();
+  //console.log(token);
+  const response =  await axios.get("http://localhost:4000/tutors");
+  //console.log(response.data);
+  t = response.data;
+  //console.log(checker);
+      }catch (error) {
+          console.log(error.message);
+      }
   
-  </section>
+  }
+    return (
+      <div>
+  <section className="page-section bg-light" id="tutorlist">
+  <h1><center>Tutoring Listing</center></h1>
+      {tutors}
+</section>
       </div>
     );
     };
--- src/tutorSignUp.js
+++ src/tutorSignUp.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const TutorSignUp = () => {
   const { logout,isAuthenticated  } = useAuth0();
   return (
