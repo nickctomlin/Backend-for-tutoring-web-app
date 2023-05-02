5d288afebdeece52e62bcd2d22e36a35c31524e0
Sreya Nagumalla <sreya.nagumalla2001@gmail.com>
2023-05-01 20:07:11 -0500

    added favorite list

---
+++ src/FavoriteList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FavoriteList = () => {
+        return (
+          <div>
+           <section>
+
+  </section>
+  </div>
+        );
+    };
+    
+    export default FavoriteList;
